07eaa88eebbef978d4fd356a12f237793fd8c616
TriggerhappyJ <jacob30wheeler@gmail.com>
2022-07-21 21:42:54 +1200

    adding in animations and falling animations

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,16 +6,23 @@ public class PlayerController : MonoBehaviour
 {
     // Stores reference to RigidBody
     private Rigidbody2D rbody;
+    private Animator anim;
 
     // Stores movement speed
     [SerializeField] private float moveSpeed;
-    [SerializeField] private float jumpForce;
+    [SerializeField] private float jumpForce = 0f;
+    [SerializeField] private float fallThreshold = -0.1f;
     private float moveX = 0f;
+
+    // Variables for checking player state
+    private bool isGrounded;
+    private bool isFalling;
 
     // Start is called before the first frame update
     void Start()
     {
         rbody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -37,12 +44,41 @@ public class PlayerController : MonoBehaviour
         // Jump
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rbody.velocity = new Vector2(rbody.velocity.x, jumpForce);
+            Jump();
         }
+ 
+        if (rbody.velocity.y < fallThreshold)
+        {
+            isFalling = true;
+        }
+        else
+        {
+            isFalling = false;
+        }
+
+        // Set Animations
+        anim.SetBool("isRunning", moveX != 0);
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetBool("isFalling", isFalling);
     }
 
-    private void FixedUpdate() {
+    private void FixedUpdate() 
+    {
         // Movement calculations
         rbody.velocity = new Vector2(moveX * moveSpeed, rbody.velocity.y);
+    }
+
+    private void Jump()
+    {
+        rbody.velocity = new Vector2(rbody.velocity.x, jumpForce);
+        isGrounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
     }
 }
